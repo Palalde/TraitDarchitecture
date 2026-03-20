34f3b15544d5ca195556085a6291d83da36b21af
Palalde <Palessandrini13@gmail.com>
2026-03-20 02:41:30 +0100

    refactor(landing page): responsive / media query

--- src/components/home/LandingScreen.tsx
+++ src/components/home/LandingScreen.tsx
@@ -3,27 +3,32 @@ import { T2ALogo } from '../ui/T2ALogo';
 export function LandingScreen() {
   return (
     <section className="relative h-screen w-full overflow-hidden bg-(--bg-primary)">
-      {/* Contenair*/}
+        {/* wrapper */}
       <div
         className="absolute top-1/2 left-1/2 flex items-start"
-        style={{ transform: 'translate(calc(var(--landing-translate) * -1), -50%)' }}
+        style={{ transform: 'translate(calc(var(--landing-center) * -1), -50%)' }}
       >
-        {/* Logo */}
+        {/* logo */}
         <T2ALogo className="w-(--landing-logo) shrink-0 text-(--trait)" />
-
-        {/* Nums + titres */}
-        <div className="flex" style={{ marginTop: 'calc(var(--landing-offset) * -1)' }}>
+        {/* text columns */}
+        <div
+          className="flex"
+          style={{ marginTop: 'calc(var(--landing-offset) * -1)' }}
+        >
+            {/* 01 / ATELIER */}
           <div className="w-(--landing-col) flex flex-col items-start">
-            <span className="font-light text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text)">01</span>
-            <span className="font-semibold text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text)">ATELIER</span>
+            <span className="font-light text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) leading-(--landing-leading)">01</span>
+            <span className="font-semibold text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) leading-(--landing-leading)">ATELIER</span>
           </div>
+          {/* 02 / TraiT */}
           <div className="w-(--landing-col) flex flex-col items-start">
-            <span className="font-light text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text)">02</span>
-            <span className="font-semibold text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text)">TraiT</span>
+            <span className="font-light text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) leading-(--landing-leading)">02</span>
+            <span className="font-semibold text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) leading-(--landing-leading)">TraiT</span>
           </div>
+          {/* 03 / D'ARCHITECTURE */}
           <div className="w-(--landing-col) flex flex-col items-start">
-            <span className="font-light text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text)">03</span>
-            <span className="font-semibold text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) whitespace-nowrap">D'ARCHITECTURE</span>
+            <span className="font-light text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) leading-(--landing-leading)">03</span>
+            <span className="font-semibold text-(--t2a-blue-dark) dark:text-(--t2a-blue-light) text-(length:--landing-text) leading-(--landing-leading)">D'ARCHITECTURE</span>
           </div>
         </div>
       </div>
