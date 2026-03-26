b2897194a2303c96eb6cbacaacd4bf28ef54591f
Palalde <Palessandrini13@gmail.com>
2026-03-26 22:54:19 +0100

    feat(layout): integrate OverlayScrollbarsSetup component and styles into BaseLayout

---
+++ src/components/ui/overlay/OverlayScrollbarsSetup.tsx
@@ -0,0 +1,9 @@
+import { useDesktopOverlayScrollbars } from "@/hooks/useDesktopOverlayScrollbars";
+
+export function OverlayScrollbarsSetup() {
+  useDesktopOverlayScrollbars();
+
+  return null;
+}
+
+export default OverlayScrollbarsSetup;
