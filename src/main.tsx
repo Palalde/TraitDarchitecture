d009f4abba18b0ad6f37dce2330e778f120067c5
Palalde <Palessandrini13@gmail.com>
2026-03-26 22:37:54 +0100

    feat(styles): update import paths for CSS files and add new styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "overlayscrollbars/styles/overlayscrollbars.css";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
